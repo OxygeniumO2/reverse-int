eade1763af82f03d865753a3f25186ab60a843e6
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2022-06-26 22:10:59 +0300

    feat: reverse int done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  n = Math.abs(n) + '';
+  for (i = 0; i < n.length; i++) {
+    result = n[i] + result;
+  }
+  return +result;
 }
+
